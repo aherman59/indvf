670f6472ff8d7289807381c7c97c8195657e90da
Antoine Herman <herman.antoine@gmail.com>
2015-12-20 21:54:42 +0100

    modification views et urls principales

--- importdvf/views.py
+++ importdvf/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+import time
+
+# Create your views here.
+
+
+def step1(request):
+    context = {'numero' : '0', 'pourcentage' : '15', 'numero_suivant':'1'}
+    return render(request, 'templates/template.html', context)
+    
+def step(request, etape):
+
+    context = None
+    print('Etape ', etape)
+    if etape =='1':
+        time.sleep(5)
+        context = {'numero' : '1', 'pourcentage' :'26', 'numero_suivant':'2'}
+    if etape =='2':
+        time.sleep(25)
+        context = {'numero' : '2', 'pourcentage' :'40', 'numero_suivant':'3'}
+    if etape =='3':
+        time.sleep(0)
+        return HttpResponse("OK")
+    return render(request, 'templates/template.html', context)
