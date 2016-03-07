17163159a1bff5a03d634cdfed588dc35256e828
Antoine HERMAN <antoine.herman@cerema.fr>
2016-03-07 17:18:49 +0100

    mise en place requeteur

--- easydvf/recherche/requetes.py
+++ easydvf/recherche/requetes.py
@@ -6,16 +6,24 @@ class Requeteur(PgOutils):
     def __init__(self, hote, base, port, utilisateur, motdepasse, script):
         super().__init__(hote, base, port, utilisateur, motdepasse, script)
     
-    def mutations(self):
-        mutations = self.recuperer_mutations()
-        return self.transformer_mutations_en_namedtuple(mutations)
-    
-    def transformer_mutations_en_namedtuple(self, mutations):
+    @classmethod
+    def transformer_mutations_en_namedtuple(cls, mutations):
         mutation_nt = namedtuple('Mutation', ['id', 'idmutinvar', 'datemut', 'valeurfonc', 'sbati', 'sterr'])
         return [mutation_nt(*mutation) for mutation in mutations]
     
-    @select_sql
-    def recuperer_mutations(self):
-        pass
+    def mutations(self, codes_insee):
+        mutations = self.recuperer_mutations(codes_insee)        
+        mutations = [list(mutation) for mutation in mutations]
+        for mutation in mutations:
+            mutation[2] = (mutation[2]).strftime("%d/%m/%Y")
+            mutation[3] = str(mutation[3])
+            mutation[4] = str(mutation[4])
+            mutation[5] = str(mutation[5])
+        mutations = self.transformer_mutations_en_namedtuple(mutations)
+        return mutations
+    
+    @select_sql_avec_modification_args
+    def recuperer_mutations(self, codes_insee):
+        return ("', '".join(codes_insee),)
         
     
--- easydvf/views.py
+++ easydvf/views.py
@@ -3,19 +3,46 @@ import json
 from django.http import HttpResponse
 from .recherche.requetes import Requeteur
 from main import configuration, controle_bdd
+from indicateur.models import Departement, Epci, Commune, Territoire
 
 # Create your views here.
 
-def recherche(request):    
-    # verification qu'une configuration a bien été definie
-    config_active = configuration.configuration_active()
-    if config_active and controle_bdd.verification_configuration(config_active):
-        params = (config_active.hote, config_active.bdd, config_active.port, config_active.utilisateur, config_active.mdp)
-        requeteur = Requeteur(*params, script = 'sorties/requeteur_recherche.sql')
-        mutations = requeteur.mutations()
-    context = {'mutations': mutations}
+def recherche(request):
+    
+    departements = Departement.objects.all()
+    # initialiation des variables de session
+    if request.method != 'POST':        
+        # verification qu'une configuration a bien été definie 
+        config_active = configuration.configuration_active()
+        if config_active and controle_bdd.verification_configuration(config_active):
+            request.session['params'] = (config_active.hote, config_active.bdd, config_active.port, config_active.utilisateur, config_active.mdp)
+        else:
+            return redirect('main:configuration_bdd')
+        request.session['departement'] = int(departements[0].pk)
+        request.session['ecpi'] = int(Epci.objects.filter(departement=request.session['departement'])[0].pk)
+        request.session['commune'] = int(Commune.objects.filter(departement=request.session['departement'])[0].pk)
+        request.session['mutations'] = []
+    # changement de département
+    if 'departement' in request.POST:
+        request.session['departement'] = int(request.POST['departement'])
+    epcis = Epci.objects.filter(departement=request.session['departement'])
+    communes = Commune.objects.filter(departement = request.session['departement'])
+    if 'voir_epci' in request.POST:
+        request.session['epci'] = int(request.POST['epci'])
+        codes_insee = [str(c.code) for c in Commune.objects.filter(epci = request.session['epci'])]       
+    if 'voir_commune' in request.POST:
+        request.session['commune'] = int(request.POST['commune'])
+        codes_insee = [str(Commune.objects.get(pk = request.session['commune']).code),]
+    if ('voir_commune' in request.POST) or ('voir_epci' in request.POST):   
+        requeteur = Requeteur(*(request.session['params']), script = 'sorties/requeteur_recherche.sql')            
+        mutations = requeteur.mutations(codes_insee)
+        request.session['mutations'] = mutations
+    else:
+        mutations = Requeteur.transformer_mutations_en_namedtuple(request.session['mutations'])
+    context = {'departements' : departements, 'epcis' : epcis, 'communes' : communes, 'mutations': mutations}
     return render(request, 'recherche.html', context)
 
 def recherche_detaillee(request, id):
     data = {'test':True}
     return HttpResponse(json.dumps(data), content_type='application/json')
+
