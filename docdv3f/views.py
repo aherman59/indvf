ab8dc28fbe1053dbacffcbddd280dd6ef6fe524b
Antoine Herman <herman.antoine@gmail.com>
2016-03-20 22:08:45 +0100

    modif doc

--- docdv3f/views.py
+++ docdv3f/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from docdv3f.models import DescriptionVariable, Variable, ValeurVariable, integration_donnees_variables
 
-# Create your views here.
-
-
 def accueil_doc(request):
     integration_donnees_variables()    
     # Dictionnaire décrivant les tables DVF+ et DV3F
