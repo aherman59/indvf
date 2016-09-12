49c03c730383b7709691e093c15dcfddffdbf4b3
Antoine HERMAN <antoine.herman@cerema.fr>
2016-09-12 17:17:34 +0200

    modification tuto

--- tutodvf/views.py
+++ tutodvf/views.py
@@ -8,8 +8,9 @@ from .gestion_fiches_tuto import obtenir_fiches_disponibles
 
 REPERTOIRE_FICHES = os.path.join(BASE_DIR, 'tutodvf', 'fiches')
 
-def accueil(request): 
-    FICHES = obtenir_fiches_disponibles(REPERTOIRE_FICHES)   
+
+def accueil(request):
+    FICHES = obtenir_fiches_disponibles(REPERTOIRE_FICHES)        
     context = {'fiches' : FICHES}
     return render(request, 'sommaire.html', context)
 
@@ -18,7 +19,7 @@ def fiche(request, nom_fichier_md):
     for i, fiche_tuto in enumerate(FICHES):
         if fiche_tuto['lien'].endswith(nom_fichier_md):
             fiche_selectionnee = fiche_tuto
-            fiche_suivante = FICHES[i+1] if i < len(FICHES) else None
+            fiche_suivante = FICHES[i+1] if i < len(FICHES) - 1 else None
             fiche_precedente = FICHES[i-1] if i > 0 else None
     context = {'fiche_selectionnee' : fiche_selectionnee,
                'fiche_suivante': fiche_suivante,
